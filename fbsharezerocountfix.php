48a0c460db60ce8cb74084e29d0312f5b66fe32e
mindgears <mindgears@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-24 10:22:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facebook-share-button-zero-count-fix/tags/1.2@350338 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fbsharezerocountfix.php
@@ -0,0 +1,85 @@
+<?php
+/*
+
+Plugin Name:  Facebook Share Button Zero Count Fix
+Plugin URI:   http://www.mindgears.de/2011/02/wordpress-plugin-facebook-share-button-zero-count-fix/
+Description:  Fixes the annoying behaviour of the Facebook share buttons not to show the counter box if something is shared less than 3 times.
+Version:      1.2
+Author:       Bernd Zolchhofer
+Author URI:   http://www.mindgears.de
+License:      GPL 2
+
+	Copyright 2011  Bernd Zolchhofer  (email : burnt@mindgears.de)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+	
+*/
+
+
+add_action('wp_head', 'fb_sharebutton_zerofix');
+add_action('admin_menu', 'fb_sharebutton_zerofix_admin_menu');
+
+function fb_sharebutton_zerofix() {
+		echo '<script type="text/javascript">' . "\n";
+		echo '	//<![CDATA[' . "\n";
+		echo '	jQuery(document).ready(function(){' . "\n";
+		echo '	jQuery(".fb_share_no_count .fb_share_count_inner").text("' . get_option('fb_sharebutton_zerofix_count') . '");' . "\n";
+		echo '	jQuery(".fb_share_no_count").removeClass("fb_share_no_count");' . "\n";
+		echo '	});' . "\n";
+		echo '	//]]>' . "\n";
+		echo '</script>' . "\n";	
+}
+
+
+
+
+
+function fb_sharebutton_zerofix_admin_menu() {
+  add_options_page('FB Share Zero Count Fix', 'FB Share Zero Fix', 'manage_options', 'facebook_sharebutton_zerocount_fix', 'fb_sharebutton_zerofix_options');
+}
+
+
+
+
+
+function fb_sharebutton_zerofix_options() {
+	
+	if (!current_user_can('manage_options')) {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+	
+	?>
+	<div class="wrap">
+		<h2>Facebook Share Button Zero Count Fix Options</h2>
+		<form method="post" action="options.php">
+			<?php wp_nonce_field('update-options'); ?>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row"><label for="option_fb_sharebutton_zerofix_count">Show this count instead of nothing:</label></th>
+					<td>
+						<select id="option_fb_sharebutton_zerofix_count" name="fb_sharebutton_zerofix_count" size="1">
+							<option<?php if (get_option('fb_sharebutton_zerofix_count') == 0) { echo ' selected'; } ?>>0</option>
+							<option<?php if (get_option('fb_sharebutton_zerofix_count') == 1) { echo ' selected'; } ?>>1</option>
+							<option<?php if (get_option('fb_sharebutton_zerofix_count') == 2) { echo ' selected'; } ?>>2</option>
+						</select>
+					</td>
+				</tr>
+			</table>
+			<input type="hidden" name="action" value="update" />
+			<input type="hidden" name="page_options" value="fb_sharebutton_zerofix_count" />
+			<p class="submit">
+				<input type="submit" value="Update Options" />
+			</p>
+			<p>Plugin by Bernd Zolchhofer - <a href="http://www.mindgears.de" target="_blank">www.mindgears.de</a></p>
+		</form>
+	</div>
+	<?php
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Facebook Share Button Zero Count Fix ===
+Contributors: mindgears
+Donate link: 
+Tags: facebook, share, button, social, meta, zero, counter
+Requires at least: 2.9.0
+Tested up to: 3.1
+Stable tag: 1.2
+
+Fixes the annoying behaviour of the Facebook share buttons not to show the counter box if something is shared less than 3 times.
+
+== Description ==
+
+You know it well: The Facebook share buttons with counter box can be an annoying thing for your blog design because the counter box doesn't show up for sharing count less than 3.
+
+This plugin fixes the problem by showing a configurable count of 0 - 2 where otherwise the whole counter box would be missing and disturbing your blog layout.
+
+== Installation ==
+
+1. Download the plugin, unzip it and upload the "facebook-share-button-zero-count-fix" folder to the "/wp-content/plugins/" directory
+2. Activate the plugin
+3. Configure the counter option
+4. Enjoy!
+
+== Frequently Asked Questions ==
+
+= Uhu, the counter number can be changed manually. Why don't this plugin allow me changing it to 13.646?  =
+
+1. Doh. Look, at first this is a big fat lie to your readers and I don't think this is cool in any way.
+2. If someone clicks your share button again, the faked number immediately disappears and shows the real count, so you're busted...
+
+== Changelog ==
+
+= 1.2 =
+* Compatibility for WP 3.1 added
+
+= 1.1 =
+* Fixed a typo in installation instructions
+
+= 1.0 =
+* Initial release
